aeb5820963c8fdb1eacc46b8e922b6f1c639664e
ppyang010 <644724460@qq.com>
2016-08-18 17:57:57 +0800

    2016年8月18日 17:57:54

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 var http = require('http');
+var url=require('url');
+
+function start(route,handle){
+	function onRequest(request,response){
+
+		var pathname=url.parse(request.url).pathname;
+		console.log('request for '+pathname+' received');
+
+		route(handle,pathname);
 
 
+		// 发送 HTTP 头部  HTTP 状态值: 200 : OK 内容类型: text/plain 
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		// 发送响应数据 "Hello World"
+		response.write('hello world\n');
+		//response.end('Hello World\n');
+		response.end();
+	}
 
-function onRequest(request,response){
-	// 发送 HTTP 头部 
-	// HTTP 状态值: 200 : OK
-	// 内容类型: text/plain
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	// 发送响应数据 "Hello World"
-	response.write('hello world\n');
-	//response.end('Hello World\n');
-	response.end();
+	http.createServer(onRequest).listen(8888);
+	// 终端打印如下信息
+	console.log('Server running at http://127.0.0.1:8888/');
 }
-
-http.createServer(onRequest).listen(8888);
-
-// 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+exports.start = start;
